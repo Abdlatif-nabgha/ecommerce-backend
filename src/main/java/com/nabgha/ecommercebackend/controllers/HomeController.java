4c11cca6a50b62057892e51d7f8f26b235859fc1
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-03-25 01:00:42 +0100

    Add model to spring index method to return name dynamically

--- src/main/java/com/nabgha/ecommercebackend/controllers/HomeController.java
+++ src/main/java/com/nabgha/ecommercebackend/controllers/HomeController.java
@@ -1,19 +1,17 @@
 package com.nabgha.ecommercebackend.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Nabgha");
+        return "index";
     }
 
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "index.html";
-    }
 }
 // we marked this class as bean: class managed by spring, spring create instance and mange it
