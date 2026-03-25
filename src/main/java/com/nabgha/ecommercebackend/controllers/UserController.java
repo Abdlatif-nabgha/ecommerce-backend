69f3348939abf277573bc409478cb8299e45aab5
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-03-25 11:29:12 +0100

    Add not found to user when fetching unexisted user

---
+++ src/main/java/com/nabgha/ecommercebackend/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.nabgha.ecommercebackend.controllers;
+
+
+import com.nabgha.ecommercebackend.entities.User;
+import com.nabgha.ecommercebackend.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping
+    // method: GET, POST,
+    public List<User> getAllUsers() {
+        List<User> users;
+        users = userRepository.findAll();
+        return users;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build(); // if not found return 404
+        }
+        return ResponseEntity.ok(user);
+    }
+}
---
+++ src/main/java/com/nabgha/ecommercebackend/entities/Message.java
@@ -0,0 +1,10 @@
+package com.nabgha.ecommercebackend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Message {
+    private String text;
+}
